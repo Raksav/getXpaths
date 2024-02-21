2da01252c6827d1473e5cf7fd1cf052e45a6cb7e
raksav <raksav86@gmail.com>
2024-02-21 23:21:51 +0530

    updated npm version

--- index.js
+++ index.js
@@ -35,38 +35,88 @@ async function findXpaths () {
     });
 
     function getXPath(element) {
-        var attributes = ['innerText','id', 'name', 'class', 'href', 'placeholder', 'role', 'title','value'];
+        var attributes = ['innerText','id', 'name', 'class', 'href', 'placeholder', 'role', 'title','value', 'type'];
+        var tagnames = ['input', 'label', 'select', 'combobox', 'tr', 'td', 'th', 'iframe', 'table'];
         var xpaths = [];
         for (var i = 0; i < attributes.length; i++) {
             var attr = attributes[i];
             var attrValue;
             if (attr === 'innerText') {
+
                 try{
                     attrValue = element.innerText;
                     attrValue = attrValue.trim();
                 }catch(err){}
 
                 if (attrValue) {
-                    xpaths.push("//" + element.tagName + "[text()='" + attrValue + "']");
+
+                    if (tagnames.includes(element.tagName.toLowerCase())) {
+                        var xp = "//" + element.tagName.toLowerCase() + "[text()='" + attrValue + "']";
+                    }else{
+                        var xp = "//*[text()='" + attrValue + "']";
+                    }
+
+                    if (isXPathUnique(xp,element)) {
+                        xpaths.push(xp);
+                    }else{
+                        xpaths.push(makeXPathUnique(xp,element));
+                    }
+
                 }else{
+
                     try{
                         attrValue = element.innerText;
                         attrValue = attrValue.trim();
                     }catch(err){}
 
                     if (attrValue) {
-                        xpaths.push("//" + element.tagName + "[text()='" + attrValue + "']");
+
+                        if (tagnames.includes(element.tagName.toLowerCase())) {
+                            var xp = "//" + element.tagName.toLowerCase() + "[text()='" + attrValue + "']";
+                        }else{
+                            var xp = "//*[text()='" + attrValue + "']";
+                        }
+
+                        if (isXPathUnique(xp,element)) {
+                            xpaths.push(xp);
+                        }else{
+                            xpaths.push(makeXPathUnique(xp,element));
+                        }
+
                     }
                 }
 
             } else {
                 attrValue = element.getAttribute(attr);
                 if (attrValue) {
-                    xpaths.push("//" + element.tagName + "[@" + attr + "='" + attrValue + "']");
-                    var alphaParts = getStaticPart(attrValue);
+
+                    if (tagnames.includes(element.tagName.toLowerCase())) {
+                        var xp = "//" + element.tagName.toLowerCase() + "[@" + attr + "='" + attrValue + "']";
+                    }else{
+                        var xp = "//*[@" + attr + "='" + attrValue + "']";
+                    }
+
+                    if (isXPathUnique(xp,element)) {
+                        xpaths.push(xp);
+                    }else{
+                        xpaths.push(makeXPathUnique(xp,element));
+                    }
+
+                    var alphaParts = getSubstringBetweenNumbers(attrValue);
                     if(alphaParts != null){
                         if (alphaParts.length > 0) {
-                            xpaths.push("//" + element.tagName + "[contains(@" + attr + ", '" + alphaParts + "')]");
+                            
+                            if (tagnames.includes(element.tagName.toLowerCase())) {
+                                var xp = "//" + element.tagName.toLowerCase() + "[contains(@" + attr + ", '" + alphaParts + "')]";
+                            }else{
+                                var xp = "//*[contains(@" + attr + ", '" + alphaParts + "')]";
+                            }
+
+                            if (isXPathUnique(xp,element)) {
+                                xpaths.push(xp);
+                            }else{
+                                xpaths.push(makeXPathUnique(xp,element));
+                            }
                         }
                     }
                 }
@@ -75,15 +125,15 @@ async function findXpaths () {
 
         if (element === document.body) return [element.tagName];
 
-        var ix = 0;
-        var siblings = element.parentNode.childNodes;
-        for (var i = 0; i < siblings.length; i++) {
-            var sibling = siblings[i];
-            if (sibling === element) {
-                xpaths.push(getXPath(element.parentNode) + '/' + element.tagName + '[' + (ix + 1) + ']');
-            }
-            if (sibling.nodeType === 1 && sibling.tagName === element.tagName) ix++;
-        }
+        // var ix = 0;
+        // var siblings = element.parentNode.childNodes;
+        // for (var i = 0; i < siblings.length; i++) {
+        //     var sibling = siblings[i];
+        //     if (sibling === element) {
+        //         xpaths.push(getXPath(element.parentNode) + '/' + element.tagName + '[' + (ix + 1) + ']');
+        //     }
+        //     if (sibling.nodeType === 1 && sibling.tagName === element.tagName) ix++;
+        // }
         return xpaths;
     }
 
@@ -96,6 +146,42 @@ async function findXpaths () {
         }catch(err){
             return null;
         }
+    }
+
+    function isXPathUnique(xpath, element) {
+        try {
+            var matchingElements = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        } catch (error) {
+            return false;
+        }
+        return matchingElements.snapshotLength === 1 && matchingElements.snapshotItem(0) === element;
+    }
+
+    function makeXPathUnique(xpath, element) {
+        try {
+            var matchingElements = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+            for (var i = 0; i < matchingElements.snapshotLength; i++) {
+                if (matchingElements.snapshotItem(i) === element) {
+                    var newXpath = "(" + xpath + ')[' + (i+1) + ']';
+                    matchingElements = document.evaluate(newXpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+                    if (matchingElements.snapshotLength === 1 && matchingElements.snapshotItem(0) === element) {
+                        return newXpath;
+                    }
+                }
+            }
+        } catch (error) {
+            return null;  // Return null if the element is not found
+        }
+    }
+    
+    function getSubstringBetweenNumbers(str) {
+        const parts = str.split(/\d+/);
+        for(let i = 0; i < parts.length; i++) {
+            if(parts[i].trim() !== '') {
+                return parts[i];
+            }
+        }
+        return '';
     }
     `;
   
